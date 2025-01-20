f662fbb904aa8b20fc898c6e7f07e3f09ecfd993
javProg24 <javiertomalag14@gmail.com>
2025-01-20 16:53:15 -0500

    Cedula

--- Data/Models/Usuario.cs
+++ Data/Models/Usuario.cs
@@ -9,6 +9,7 @@ namespace Data.Models
     public class Usuario
     {
         public int Id { get; set; }
+        public string Cedula { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Correo { get; set; }
